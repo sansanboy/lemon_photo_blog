a46360bb9170dbbcbab0c2200ce1d9e409c2e57e
jimyan <690817315@qq.com>
2026-01-06 15:49:21 +0800

    fix next

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      }
-    ]
-  }
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+    outputFileTracing: false,
 };
 
 export default nextConfig;
